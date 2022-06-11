4c3049c8f8c764e7ad41185276b81568da8b0050
Furkan Yıldırım <yildirim.frkn@outlook.com>
2022-06-11 19:03:01 +0300

    view ler eklendi

---
+++ eShopOnContainers/eShopOnContainers.Core/Views/Account.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace eShopOnContainers.Core.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Account : ContentPage
+    {
+        public Account()
+        {
+            InitializeComponent();
+        }
+        private async void Button_Kayit(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Register());
+        }
+
+        private async void Button_Giris(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Login());
+        }
+    }
+}
