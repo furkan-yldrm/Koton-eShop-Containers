e2116e492eea29cdf9c420e894999cf8d0570e9d
Furkan Yıldırım <yildirim.frkn@outlook.com>
2022-06-13 01:47:40 +0300

    Giris view model eklendi

---
+++ eShopOnContainers/eShopOnContainers.Core/ViewModels/GirisViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace eShopOnContainers.Core.ViewModels
+{
+    public class GirisViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
